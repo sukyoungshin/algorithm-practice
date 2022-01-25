3b544a0e4b1711de96a59e14b41a3c436855a2a9
sukyoung shin <sukyoung.dev@gmail.com>
2022-01-26 05:27:33 +0900

    doc : reduce() 설명추가

--- 13.js
+++ 13.js
@@ -96,8 +96,21 @@ function solution() {
 
 console.log(solution());
 
+// ================================ //
 // reduce
+// - 비용고려하여 map을 사용 할 수 있는 경우엔 map을 사용한다. 굳이 reduce를 고집할 필요없다.
+// - reduce를 사용하는 경우 : 배열로 주는 값을 카운트해줄때, 배열이 아닌 다른 값을 return할 때, map이나 filter 체이닝 걸어서 할거 reduce로 한방에 가능할 때
+
 // const reducer = (acc, curr) => {
 //   return acc + (date === curr); // true는 1, false는 0
 // };
-// console.log( carEndNumbers.reduce(reducer, 0));
+// console.log( carEndNumbers.reduce(reducer, 0)); // 리듀스 initialValue를 생략하지 않도록 한다 (예외 발생이 되지 않도록)
+
+// ex) 
+// const Array = ['A','A','C','B','G','A','E','V','C','C','J','E','V','Q','L','Z','I','S','A','Z','O','P','O','H','I'];
+// const exampleObj = Array.reduce((acc, cur)=>{
+//   acc[cur] ? acc[cur] ++ : acc[cur] = 1;
+//   return acc;
+// }, {});
+
+// console.log(exampleObj); 
