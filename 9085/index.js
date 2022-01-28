637c2e20af3c3c54a4a87d029ee3966fc2239cdd
sukyoung shin <sukyoung.dev@gmail.com>
2022-01-29 08:52:40 +0900

    doc : 다른 풀이방법 추가

--- 9085/index.js
+++ 9085/index.js
@@ -1,18 +1,39 @@
 // https://www.acmicpc.net/problem/9085
 
-const fs = require('fs');
-const input = fs.readFileSync('./input1.txt').toString().split('\n');
-const ListOfNumbersArray = [];
+// 2 way
+const input = require('fs')
+  .readFileSync('./input1.txt')
+  .toString()
+  .split('\n');
 
-function classification(input) {
-  for (let i = 1 ; i < input.length; i++) {
-    if (i % 2 === 0) {
-      ListOfNumbersArray.push(input[i]);
-    }
-  }
+let numberOfTestCase = parseInt(input.shift()); // 2
+
+while(numberOfTestCase--) {
+  parseInt(input.shift());
+  const numbers = input.shift().split(' ').map(v => parseInt(v));
+  const sum = numbers.reduce((result, acc) => {return result + acc}, 0);
+  console.log(sum);
 };
-classification(input);
-const reducer = (prev, curr) => prev + curr;
-for (let i = 0; i < ListOfNumbersArray.length; i++) {
-  console.log( ListOfNumbersArray[i].split(' ').map(str => Number(str)).reduce(reducer) )
-};
+
+// 1 way
+// const input = require('fs')
+//   .readFileSync('./input1.txt')
+//   .toString()
+//   .split('\n');
+
+// const ListOfNumbersArray = [];
+
+// function classification(input) {
+//   for (let i = 1 ; i < input.length; i++) {
+//     if (i % 2 === 0) {
+//       ListOfNumbersArray.push(input[i]);
+//     }
+//   }
+// };
+// classification(input);
+
+// const reducer = (prev, curr) => prev + curr;
+// for (let i = 0; i < ListOfNumbersArray.length; i++) {
+//   console.log( ListOfNumbersArray[i].split(' ').map(str => Number(str)).reduce(reducer) )
+// };
+
