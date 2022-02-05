158040a9199ab8d9d253a5244eb5e6ab2d9cdbac
sukyoung shin <sukyoung.dev@gmail.com>
2022-02-05 09:27:32 +0900

    refactor : 기존코드 리펙터링

--- 5354/index.js
+++ 5354/index.js
@@ -1,20 +1,22 @@
 // https://www.acmicpc.net/problem/5354
 
-const inputs = require('fs')
-  .readFileSync('./input1.txt')
-  .toString()
-  .trim()
-  .split('\n'); 
-const testCase = parseInt(inputs[0]); // 3  
-inputs.shift();  
-const sizeOfJbox = inputs.map(v => parseInt(v)) // [3, 5, 4]
+function init() {
+  const inputs = require('fs')
+    .readFileSync('./input1.txt')
+    .toString()
+    .trim()
+    .split('\n'); 
+  const testCase = parseInt(inputs[0]); // 3  
+  inputs.shift();  
+  const sizeOfJbox = inputs.map(v => parseInt(v)) // [3, 5, 4]
 
-function createJbox(size){
-  const firstLine = 0;
-  const lastLine = size - 1;
+  return [testCase, sizeOfJbox];
+};
+
+function createEachJbox(size){
   const array = [];
   for (let i = 0; i < size; i++) {
-    if (i === firstLine || i === lastLine) {
+    if (i === 0 || i === size - 1) {
       array.push('#'.repeat(size)); 
     } else {
       array.push('#' + 'J'.repeat(size - 2) + '#')
@@ -23,30 +25,31 @@ function createJbox(size){
   return array;
 };
 
-function getResultOfEachJbox() {
-  const resultOfJbox = [];
+function getArrayOfJboxes(testCase, jBoxSize) {
+  const resultArray = [];
   for (let i = 0; i < testCase; i++) {
-    const eachJbox = createJbox(sizeOfJbox[i]); 
-    resultOfJbox.push(eachJbox);
+    const eachJbox = createEachJbox(jBoxSize[i]); 
+    resultArray.push(eachJbox);
   }
-  return resultOfJbox;
+  return resultArray;
 };
 
-function getResultOfTotalJbox(resultOfEachJbox) {
-  for (let i = 0; i < resultOfEachJbox.length; i++) {
-    if (i === resultOfEachJbox.length - 1) {
-      resultOfEachJbox[i].map(v => console.log(v));
-    } else {
-      resultOfEachJbox[i].map(v => console.log(v));
+function getResultOfTotalJbox(arrayOfJboxes) {
+  for (let i = 0; i < arrayOfJboxes.length; i++) {
+    if (i !== arrayOfJboxes.length - 1) {
+      arrayOfJboxes[i].map(v => console.log(v));
       console.log('');
+    } else {
+      arrayOfJboxes[i].map(v => console.log(v));
     }
   }
 };
 
 function solution() {
-  const resultOfEachJbox = getResultOfEachJbox(); // ['###', '#J#', '###']
-  const resultOfTotalJbox = getResultOfTotalJbox(resultOfEachJbox);
-  return resultOfTotalJbox;
+  const [ testCase, sizeOfJbox ] = init();
+  const arrayOfJbox = getArrayOfJboxes(testCase, sizeOfJbox); 
+  const totalJbox = getResultOfTotalJbox(arrayOfJbox);
+  return totalJbox;
 };
 solution();
-// 생각 쪼개셈
+// 생각 쪼개기
