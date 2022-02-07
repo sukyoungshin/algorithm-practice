9dbcd734bfd15a80e1516fbd3e142697076b99bc
sukyoung shin <sukyoung.dev@gmail.com>
2022-02-07 11:34:36 +0900

    refactor : 기존코드 리펙터링

--- 15.js
+++ 15.js
@@ -1,56 +1,48 @@
 // 1. 학급 회장(해쉬)
-
 function init() {
   const numberOfStudents = 15;
-  const candidates = ['A', 'B', 'C', 'D', 'E'];
-  const resultOfVote = 'BACBACCACCBDEDE'.split(''); // ['B', 'A', 'C', 'B', 'A', 'C', 'C', 'A', 'C', 'C', 'B', 'D', 'E', 'D', 'E']
-  return { numberOfStudents, candidates, resultOfVote };
+  const currentCandidates = 'BACBACCACCBDEDE'.split(''); // ['A', 'A', 'B', 'E', 'D']
+  const expectedCandidates = ['A', 'B', 'C', 'D', 'E'];
+  return {numberOfStudents, currentCandidates, expectedCandidates};
 };
 
-function createCandidateObj(candidates) {
-  const countObject = {};
+function convertArrayToObject(candidates) {
+  const obj = {};
   for (let i = 0; i < candidates.length; i++) {
-    const keyValue = candidates[i];
-    countObject[keyValue] = 0;
+    const keys = candidates[i];
+    obj[keys] = 0;
   }
-  return countObject;
+  return obj;
 };
 
-function countResultOfVote(candidateObj, resultOfVote) {
-  const A = 'A', B = 'B', C = 'C', D = 'D', E = 'E';
+function countResultOfVote(candidates, resultOfVote) {
   for (let i = 0; i < resultOfVote.length; i++) {
-    if (resultOfVote[i] === A) {
-      candidateObj[A] += 1;
-    } else if (resultOfVote[i] === B) {
-      candidateObj[B] += 1;
-    } else if (resultOfVote[i] === C) {
-      candidateObj[C] += 1;
-    } else if (resultOfVote[i] === D) {
-      candidateObj[D] += 1;
-    } else {
-      candidateObj[E] += 1;
-    }
+    let keys = resultOfVote[i];
+    candidates[keys] += 1; 
   }
-  return candidateObj;
+  return candidates;
 };
 
+// for in으로 바꿔보기
 function getResult(countObj) {
-  const objectValueLength = Object.values(countObj);
-  const objectKeys = Object.keys(countObj);
-  const max = objectValueLength[0];
-  for (let i = 0; i < objectValueLength.length; i++) {
-    if ( max < objectValueLength[i]) {
-      console.log(objectKeys[i]);
-    }
+  const objectKeys = Object.keys(countObj); 
+  const objectValue = Object.values(countObj); 
+  let student = objectKeys[0];
+  let max = objectValue[0]; // 엣지케이스 (objectValue[0]이 제일 큰 값인 경우)
+  for (let i = 0; i < objectValue.length; i++) {
+    if ( max < objectValue[i]) {
+      max = objectValue[i];
+      student = objectKeys[i];
+    } 
   }
+  return student;
 };
 
 function solution() {
-  const { candidates, resultOfVote} = init();
-  const candidateObj = createCandidateObj(candidates); // { A: 0, B: 0, C: 0, D: 0, E: 0 }
-  const count = countResultOfVote(candidateObj, resultOfVote); // { A: 3, B: 3, C: 5, D: 2, E: 2 }
-  const result = getResult(count); // C
+  const { currentCandidates, expectedCandidates} = init(); 
+  const candidateObj = convertArrayToObject(expectedCandidates); 
+  const count = countResultOfVote(candidateObj, currentCandidates);
+  const result = getResult(count); 
   return result;
 };
-
-solution();
+console.log(solution());
