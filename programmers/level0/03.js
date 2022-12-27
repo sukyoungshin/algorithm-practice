33d3775d8be8a1716fd03d9f5a785ab7e0884b30
SuKyoung <87718391+sukyoungshin@users.noreply.github.com>
2022-12-27 20:54:58 +0900

    문제풀이 수정

--- programmers/level0/03.js
+++ programmers/level0/03.js
@@ -4,37 +4,13 @@
 // 1
 function solution(s1, s2) {
     let answer = 0;
-    // s1, s2 두 배열의 길이가 같을 때
-    if (s1.length === s2.length) {
-        for (let i = 0; i < s1.length; i++) {
-            if (s2.includes(s1[i])) {
-                answer++
-            } 
-        }  
-        return answer;
-    }
-    
-    // s1, s2 두 배열의 길이가 다를 때
-    const longArray = getLongArray(s1, s2); // 긴 배열
-    const shortArray = getShortArray(s1, s2); // 짧은 배열
-
-    for (let i = 0; i < shortArray.length; i++) {
-      if (!longArray.includes(shortArray[i])) continue;
-      if (longArray.includes(shortArray[i])) {
-          answer++;
-      }
-    }
-
+    for (let i = 0; i < s1.length; i++) {
+        if (s2.includes(s1[i])) {
+            answer++
+        } 
+    }  
     return answer;
-}
-
-const getLongArray = (s1, s2) => {
-    return s1.length > s2.length ? s1 : s2;
-}
-
-const getShortArray = (s1, s2) => {
-    return s1.length < s2.length ? s1 : s2;
-}
+};
 
 
 // 2
