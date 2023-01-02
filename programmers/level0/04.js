73b271c737a0fdccfc03f4684ca695d10e0fb114
SuKyoung <87718391+sukyoungshin@users.noreply.github.com>
2023-01-03 00:13:23 +0900

    프로그래머스 level0 문제풀이 추가

---
+++ programmers/level0/04.js
@@ -0,0 +1,28 @@
+// 피자 나눠 먹기 (1)
+
+// 1
+function solution(n) {
+    // 사람이 7명 이하일 때
+    if (n <= 7) return 1;
+
+    // 사람이 7명을 초과할 때
+    if (n > 7) {
+     if (n % 7 === 0) return n / 7;
+      return Math.floor(n / 7) + 1;
+    }
+}
+
+// 2 
+function solution(n) {
+    if (n <= 7) return 1;
+
+    if (n % 7 === 0) return n / 7;
+    return Math.floor(n / 7) + 1;
+}
+
+// 3
+function solution(n) {
+    if (n <= 7) return 1;
+
+    return (n % 7 === 0) ? n / 7 : Math.floor(n / 7) + 1;
+}
